34fcaa6107f92407631033ced232c8413843c76d
huyhoanFithcmus <huyhoan652002@gmail.com>
2022-12-10 01:22:47 +0700

    add files classes

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express()
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express()
 
-const ejs = require('ejs');
-app.set('view engine', 'ejs');
-try
-{
-mongoose.connect('mongodb+srv://huyhoan652002:huyhoan65@riotcluster.p3qr5ga.mongodb.net/test')}
-catch (error)
-{
-    console.log(error.message)
-}
+// const ejs = require('ejs');
+// app.set('view engine', 'ejs');
+// try
+// {
+// mongoose.connect('mongodb+srv://huyhoan652002:huyhoan65@riotcluster.p3qr5ga.mongodb.net/riotHCMUS?retryWrites=true&w=majority')}
+// catch (error)
+// {
+//     console.log(error.message)
+// }
 
-app.get('/', (req, res) => {
-    res.send('working')
-})
+// app.get('/', (req, res) => {
+//     res.send('working')
+// })
 
-app.listen(4000, function(){
-    console.log('server is running');
-})
+// app.listen(4000, function(){
+//     console.log('server is running');
+// })
+ 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://huyhoan652002:huyhoan65@riotcluster.p3qr5ga.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("riotHCMUS").collection("courses");
+  // perform actions on the collection object
+  client.close();
+});
+
